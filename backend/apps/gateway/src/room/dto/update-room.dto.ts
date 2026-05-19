08b4e168bb2074dc03345dd6da120c77491a88bb
Bastos <record.bastien@gmail.com>
2026-05-19 11:39:11 +0200

    docs: room docs

---
+++ backend/apps/gateway/src/room/dto/update-room.dto.ts
@@ -0,0 +1,21 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+
+export class UpdateRoomDto {
+  @ApiPropertyOptional({ example: "A101" })
+  name?: string;
+
+  @ApiPropertyOptional({ example: 25 })
+  capacity?: number;
+
+  @ApiPropertyOptional({ example: 2 })
+  floor?: string;
+
+  @ApiPropertyOptional({ example: 1, enum: [0, 1] })
+  state?: number;
+
+  @ApiPropertyOptional({ example: "Aile nord - Proche ascenseur" })
+  location?: string;
+
+  @ApiPropertyOptional({ example: 1 })
+  buildingId?: number;
+}
